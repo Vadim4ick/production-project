6d989c5197609c39a1c11412e4ad31afa31a6385
Vadim Firulyov <firulvv@mail.ru>
2023-02-28 15:58:58 +0500

    fiiix loki

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -1,4 +1,5 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+//test
 // import { Theme } from 'app/providers/ThemeProvider';
 // import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 
