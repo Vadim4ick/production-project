c885c3ba195545be86a957451c3c3882ba00b198
Vadim Firulyov <firulvv@mail.ru>
2023-02-28 18:26:28 +0500

    add testing slice, loginByUserName

---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,21 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginSchema } from '../types/loginSchema';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('loginSlice.test', () => {
+  test('test set username', () => {
+    const state: DeepPartial<LoginSchema> = { username: '321' };
+
+    expect(
+      loginReducer(state as LoginSchema, loginActions.setUsername('123')),
+    ).toEqual({ username: '123' });
+  });
+
+  test('test set password', () => {
+    const state: DeepPartial<LoginSchema> = { password: '321' };
+
+    expect(
+      loginReducer(state as LoginSchema, loginActions.setPassword('123')),
+    ).toEqual({ password: '123' });
+  });
+});
