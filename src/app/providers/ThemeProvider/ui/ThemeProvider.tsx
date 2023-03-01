9ad2998a47fc307409f0a34ecd382f2bcef81c6e
Vadim Firulyov <firulvv@mail.ru>
2023-03-01 20:37:23 +0500

    rm overlay testing

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -6,7 +6,7 @@ import {
 } from '../lib/ThemeContext';
 
 const defaultTheme =
-  (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.DARK;
+  (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
 
 interface ThemeProviderProps {
   initialTheme?: Theme;
