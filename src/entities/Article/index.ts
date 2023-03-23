737ee832b4f13201bd32f3f0d7e34e78e9e455d0
Vadim Firulyov <firulvv@mail.ru>
2023-03-23 18:30:06 +0500

    fix small

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -1,4 +1,5 @@
 export { ArticleList } from './ui/ArticleList/ArticleList';
 export { ArticleDetailsSchema } from './model/types/articleDetailsSchema';
 export { Article, ArticleView } from './model/types/article';
+
 export { ArticleDetails } from './ui/ArticleDetails/ArticleDetails';
