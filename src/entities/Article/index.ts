26e5fd7d8d327c0657332ee1e713735c0052c6ce
Vadim Firulyov <firulvv@mail.ru>
2023-03-29 20:28:53 +0500

    test

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -1,8 +1,6 @@
 export { getArticleDetailsData } from 'entities/Article/model/selectors/ArticleDetails';
 export { ArticleType } from './model/types/article';
-
 export { ArticleList } from './ui/ArticleList/ArticleList';
 export { ArticleDetailsSchema } from './model/types/articleDetailsSchema';
 export { Article, ArticleView, ArticleSortField } from './model/types/article';
-
 export { ArticleDetails } from './ui/ArticleDetails/ArticleDetails';
