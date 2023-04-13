cd149a2234e0efb5d0fbf7f82618beb5b867cf62
Vadim Firulyov <firulvv@mail.ru>
2023-04-13 15:16:03 +0500

    fix

--- src/pages/ArticleDetailesPage/index.ts
+++ src/pages/ArticleDetailesPage/index.ts
@@ -1,7 +1,6 @@
 export { ArticleDetailsPageRecommendationsSchema } from './model/types/ArticleDetailsPageRecommendationsSchema';
 export { ArticleDetailsCommentsSchema } from './model/types/ArticleDetailsCommentsSchema';
 export { ArticleDetailesPageAsync as ArticleDetailesPage } from './ui/ArticleDetailesPage/ArticleDetailesPage.async';
-
 export {
   articleDetailsCommentsReducer,
   getArticleComments,
